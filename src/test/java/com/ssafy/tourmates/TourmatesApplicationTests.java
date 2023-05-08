47710da7e26cab86e141e6d38cd9ec1e36d96ffc
Dong82 <lyt1228@naver.com>
2023-05-08 19:17:33 +0900

    init: setup tourmates project

---
+++ src/test/java/com/ssafy/tourmates/TourmatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tourmates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourmatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
