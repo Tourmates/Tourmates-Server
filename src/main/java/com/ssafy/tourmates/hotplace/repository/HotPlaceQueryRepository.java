639235734cad5ba77088c6d52e064ff2ab890d08
Chaos0103 <lyt1228@naver.com>
2023-05-10 11:59:02 +0900

    refactor: 핫플레이스 페이징 조회 쿼리 최적화

---
+++ src/main/java/com/ssafy/tourmates/hotplace/repository/HotPlaceQueryRepository.java
@@ -0,0 +1,78 @@
+package com.ssafy.tourmates.hotplace.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.tourmates.common.domain.ContentType;
+import com.ssafy.tourmates.controller.dto.hotplace.response.HotPlaceResponse;
+import com.ssafy.tourmates.hotplace.HotPlace;
+import com.ssafy.tourmates.hotplace.repository.dto.HotPlaceSearchCondition;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
+
+import javax.persistence.EntityManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.ssafy.tourmates.hotplace.QHotPlace.*;
+import static org.springframework.util.StringUtils.*;
+
+@Repository
+public class HotPlaceQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public HotPlaceQueryRepository(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<HotPlaceResponse> searchByCondition(HotPlaceSearchCondition condition, Pageable pageable) {
+        List<Long> ids = queryFactory
+                .select(hotPlace.id)
+                .from(hotPlace)
+                .where(
+                        isTag(condition.getTag()),
+                        isTitle(condition.getTitle()),
+                        isContent(condition.getContent())
+                )
+                .orderBy(hotPlace.createdDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        if (CollectionUtils.isEmpty(ids)) {
+            return new ArrayList<>();
+        }
+
+        return queryFactory
+                .select(
+                        Projections.fields(HotPlaceResponse.class,
+                                hotPlace.id,
+                                hotPlace.tag,
+                                hotPlace.title,
+                                hotPlace.content,
+                                hotPlace.hit,
+                                hotPlace.visitedDate,
+                                hotPlace.images.get(0).uploadFile.storeFileName))
+                .from(hotPlace)
+                .where(hotPlace.id.in(ids))
+                .orderBy(hotPlace.createdDate.desc())
+                .fetch();
+    }
+
+    private BooleanExpression isTag(ContentType tag) {
+        return tag != null ? hotPlace.tag.eq(tag) : null;
+    }
+
+    private BooleanExpression isTitle(String title) {
+        return hasText(title) ? hotPlace.title.like("%" + title + "%") : null;
+    }
+
+    private BooleanExpression isContent(String content) {
+        return hasText(content) ? hotPlace.content.eq("%" + content + "%") : null;
+    }
+}
