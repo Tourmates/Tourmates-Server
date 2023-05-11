4037b47a21af3397108e29a30540383aa1f1e661
Chaos0103 <lyt1228@naver.com>
2023-05-11 10:54:07 +0900

    feat: 공지사항 전체 조회 서비스 구현

---
+++ src/main/java/com/ssafy/tourmates/notice/service/NoticeQueryService.java
@@ -0,0 +1,14 @@
+package com.ssafy.tourmates.notice.service;
+
+import com.ssafy.tourmates.controller.dto.notice.response.NoticeResponse;
+import com.ssafy.tourmates.notice.repository.dto.NoticeSearchCondition;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+public interface NoticeQueryService {
+
+    List<NoticeResponse> searchByCondition(NoticeSearchCondition condition, Pageable pageable);
+}
