aab5940230893479d46a68eaf9349aecfa58245c
Chaos0103 <lyt1228@naver.com>
2023-05-11 09:34:37 +0900

    feat: 공지사항 수정 컨트롤러 구현

--- src/main/java/com/ssafy/tourmates/controller/NoticeController.java
+++ src/main/java/com/ssafy/tourmates/controller/NoticeController.java
@@ -1,13 +1,16 @@
 package com.ssafy.tourmates.controller;
 
 import com.ssafy.tourmates.controller.dto.notice.request.AddNoticeRequest;
+import com.ssafy.tourmates.controller.dto.notice.request.EditNoticeRequest;
 import com.ssafy.tourmates.jwt.SecurityUtil;
 import com.ssafy.tourmates.notice.service.NoticeService;
 import com.ssafy.tourmates.notice.service.dto.AddNoticeDto;
+import com.ssafy.tourmates.notice.service.dto.EditNoticeDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +36,19 @@ public class NoticeController {
                 .build();
 
         return noticeService.registerNotice(loginId, dto);
+    }
+
+    @ApiOperation(value = "공지사항 수정")
+    @PostMapping("/{noticeId}/edit")
+    public Long editNotice(@PathVariable Long noticeId, EditNoticeRequest request) {
+        EditNoticeDto dto = EditNoticeDto.builder()
+                .pin(request.getPin())
+                .title(request.getTitle())
+                .content(request.getContent())
+                .build();
+        Long editNoticeId = noticeService.editNotice(noticeId, dto);
+        log.debug("request={}", request);
+        log.debug("editNoticeId={}", editNoticeId);
+        return editNoticeId;
     }
 }
