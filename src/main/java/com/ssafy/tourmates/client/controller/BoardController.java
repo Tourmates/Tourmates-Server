aa54803c170576afbc71732f370527a86d90e7b9
Chaos0103 <lyt1228@naver.com>
2023-05-12 09:50:48 +0900

    feat: 게시글 전체 조회 컨트롤러 구현

--- src/main/java/com/ssafy/tourmates/client/controller/BoardController.java
+++ src/main/java/com/ssafy/tourmates/client/controller/BoardController.java
@@ -1,18 +1,26 @@
 package com.ssafy.tourmates.client.controller;
 
+import com.ssafy.tourmates.client.board.repository.dto.BoardSearchCondition;
+import com.ssafy.tourmates.client.board.repository.dto.Sort;
+import com.ssafy.tourmates.client.board.service.BoardQueryService;
 import com.ssafy.tourmates.client.board.service.BoardService;
 import com.ssafy.tourmates.client.board.service.dto.AddBoardDto;
 import com.ssafy.tourmates.client.board.service.dto.EditBoardDto;
 import com.ssafy.tourmates.client.controller.dto.board.request.AddBoardRequest;
 import com.ssafy.tourmates.client.controller.dto.board.request.EditBoardRequest;
+import com.ssafy.tourmates.client.controller.dto.board.response.BoardResponse;
 import com.ssafy.tourmates.jwt.SecurityUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +30,26 @@ import javax.validation.Valid;
 public class BoardController {
 
     private final BoardService boardService;
+    private final BoardQueryService boardQueryService;
+
+    @ApiOperation(value = "게시판 조회")
+    @GetMapping
+    public ResultPage<List<BoardResponse>> searchBoards(
+            @RequestParam(defaultValue = "") String keyword,
+            @RequestParam(defaultValue = "CREATED_DATE") Sort sort,
+            @RequestParam(defaultValue = "1") Integer pageNumber
+    ) {
+        BoardSearchCondition condition = BoardSearchCondition.builder()
+                .keyword(keyword)
+                .sort(sort)
+                .build();
+        PageRequest pageRequest = PageRequest.of(pageNumber, 20);
+        List<BoardResponse> responses = boardQueryService.searchByCondition(condition, pageRequest);
+        log.debug("responses size={}", responses.size());
+        log.debug("condition={}", condition);
+        log.debug("pageNumber={}", pageNumber);
+        return new ResultPage<>(responses, pageNumber, 20);
+    }
 
     @ApiOperation(value = "게시판 등록")
     @PostMapping("/register")
@@ -48,5 +76,13 @@ public class BoardController {
         log.debug("request={}", request);
         log.debug("editedBoardId={}", editedBoardId);
         return editedBoardId;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class ResultPage<T> {
+        private T data;
+        private int pageNumber;
+        private int pageSize;
     }
 }
