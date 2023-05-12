e19e693e723a05888bb5a3126fe52833ac345300
youbeen2798 <youbeen2798@naver.com>
2023-05-12 17:03:10 +0900

    feat: HotPlaceLikeRepository 좋아요 기능 추가

---
+++ src/main/java/com/ssafy/tourmates/client/hotplace/repository/HotPlaceLikeRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.tourmates.client.hotplace.repository;
+
+import com.ssafy.tourmates.client.hotplace.HotPlaceLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HotPlaceLikeRepository extends JpaRepository<HotPlaceLike, Long> {
+}
