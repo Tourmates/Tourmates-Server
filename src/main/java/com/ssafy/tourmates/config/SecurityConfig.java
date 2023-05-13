02a7f11b684d45115f8f24d494cb3757c86eb00b
chaos0103 <lyt1228@naver.com>
2023-05-13 15:20:33 +0900

    feat: security 설정

--- src/main/java/com/ssafy/tourmates/config/SecurityConfig.java
+++ src/main/java/com/ssafy/tourmates/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.ssafy.tourmates.config;
 
+import com.ssafy.tourmates.jwt.JwtAuthenticationFilter;
 import com.ssafy.tourmates.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,16 +24,16 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
-                .csrf().disable();
-//                .csrf().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/login", "/join", "/notice").permitAll()
-//                .antMatchers("/test").hasRole("MEMBER")
-//                .anyRequest().authenticated()
-//                .and()
-//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+//                .csrf().disable();
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login", "/join", "/notices", "/boards").permitAll()
+                .antMatchers("/boards/register").hasRole("MEMBER")
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
--- src/main/java/com/ssafy/tourmates/data/InitData.java
+++ src/main/java/com/ssafy/tourmates/data/InitData.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import javax.annotation.PostConstruct;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static com.ssafy.tourmates.client.member.Gender.*;
@@ -25,8 +26,8 @@ public class InitData {
 
     private void initMember() {
         List<Member> members = new ArrayList<>();
-        Member member1 = Member.builder().id(1L).loginId("ssafy1").loginPw("ssafy1111!").name("임우택").email("ssafy1@ssafy.com").tel("010-1111-1111").birth("2000-01-01").gender(MALE).nickname("코코").build();
-        Member member2 = Member.builder().id(2L).loginId("ssafy2").loginPw("ssafy2222!").name("정유빈").email("ssafy2@ssafy.com").tel("010-2222-2222").birth("2000-02-02").gender(FEMALE).nickname("광주").build();
+        Member member1 = Member.builder().id(1L).loginId("ssafy1").loginPw("ssafy1111!").name("임우택").email("ssafy1@ssafy.com").tel("010-1111-1111").birth("2000-01-01").gender(MALE).nickname("코코").roles(Collections.singletonList("MEMBER")).build();
+        Member member2 = Member.builder().id(2L).loginId("ssafy2").loginPw("ssafy2222!").name("정유빈").email("ssafy2@ssafy.com").tel("010-2222-2222").birth("2000-02-02").gender(FEMALE).nickname("광주").roles(Collections.singletonList("MEMBER")).build();
         members.add(member1);
         members.add(member2);
         memberRepository.saveAll(members);
