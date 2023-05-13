82151e2069db95395d113a2f04548cab41f6ffe1
chaos0103 <lyt1228@naver.com>
2023-05-13 14:01:16 +0900

    feat: 로그인 서비스 구현

--- src/main/java/com/ssafy/tourmates/client/member/service/MemberQueryService.java
+++ src/main/java/com/ssafy/tourmates/client/member/service/MemberQueryService.java
@@ -1,9 +1,12 @@
 package com.ssafy.tourmates.client.member.service;
 
+import com.ssafy.tourmates.jwt.TokenInfo;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 public interface MemberQueryService {
+
+    TokenInfo login(String loginId, String loginPw);
 
     String forgotLoginIdByEmail(String email);
 
--- src/main/java/com/ssafy/tourmates/client/member/service/impl/MemberQueryServiceImpl.java
+++ src/main/java/com/ssafy/tourmates/client/member/service/impl/MemberQueryServiceImpl.java
@@ -3,7 +3,12 @@ package com.ssafy.tourmates.client.member.service.impl;
 import com.ssafy.tourmates.client.member.service.MemberQueryService;
 import com.ssafy.tourmates.client.member.validator.MemberValidator;
 import com.ssafy.tourmates.client.member.Member;
+import com.ssafy.tourmates.jwt.JwtTokenProvider;
+import com.ssafy.tourmates.jwt.TokenInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
@@ -12,7 +17,16 @@ import java.util.NoSuchElementException;
 @RequiredArgsConstructor
 public class MemberQueryServiceImpl implements MemberQueryService {
 
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
     private final MemberValidator memberValidator;
+
+    @Override
+    public TokenInfo login(String loginId, String loginPw) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginId, loginPw);
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        return jwtTokenProvider.generateToken(authentication);
+    }
 
     @Override
     public String forgotLoginIdByEmail(String email) {
