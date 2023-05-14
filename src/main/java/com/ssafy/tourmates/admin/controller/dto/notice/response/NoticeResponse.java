66b48adf1c3893077110de7ee164c01fd570b150
chaos0103 <lyt1228@naver.com>
2023-05-14 19:04:58 +0900

    refactor: 공지사항 전체 조회 쿼리 수정

--- src/main/java/com/ssafy/tourmates/admin/controller/dto/notice/response/NoticeResponse.java
+++ src/main/java/com/ssafy/tourmates/admin/controller/dto/notice/response/NoticeResponse.java
@@ -3,11 +3,18 @@ package com.ssafy.tourmates.admin.controller.dto.notice.response;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class NoticeResponse {
 
     private Long noticeId;
     private String title;
-    private LocalDateTime createdDate;
+    private String createdDate;
+
+    public NoticeResponse(Long noticeId, String title, LocalDateTime createdDate) {
+        this.noticeId = noticeId;
+        this.title = title;
+        this.createdDate = createdDate.format(DateTimeFormatter.ofPattern("yyy-MM-dd"));
+    }
 }
