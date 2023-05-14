7d821d0243eb32604d6f61a45f46bec008430f93
정유빈 <youbeen2798@naver.com>
2023-05-15 00:34:40 +0900

    feat: AddTripPlanDto 클래스 작성

---
+++ src/main/java/com/ssafy/tourmates/client/tripPlan/service/dto/AddTripPlanDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.tourmates.client.tripPlan.service.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class AddTripPlanDto {
+
+  private String title;
+
+  @Builder
+  public AddTripPlanDto(String title) {
+    this.title = title;
+  }
+
+}
