399db2e1c7f1d67edd298267d6780a210614bf3b
정유빈 <youbeen2798@naver.com>
2023-05-15 00:36:28 +0900

    feat: TripPlanController 여행계획 등록 기능 구현

---
+++ src/main/java/com/ssafy/tourmates/client/controller/TripPlanController.java
@@ -0,0 +1,40 @@
+package com.ssafy.tourmates.client.controller;
+
+import com.ssafy.tourmates.client.controller.dto.tripPlan.AddTripPlanRequest;
+import com.ssafy.tourmates.client.tripPlan.service.TripPlanService;
+import com.ssafy.tourmates.client.tripPlan.service.dto.AddTripPlanDto;
+import com.ssafy.tourmates.jwt.SecurityUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.graalvm.compiler.core.common.type.ArithmeticOpTable.BinaryOp.Add;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/trips")
+@Api(tags = {"여행"})
+public class TripPlanController {
+
+  private final TripPlanService tripPlanService;
+
+  @ApiOperation(value = "여행계획 등록")
+  @PostMapping("/register")
+  public Long registerTripPlan(@Valid @RequestBody AddTripPlanRequest request){
+    String loginId = SecurityUtil.getCurrentLoginId();
+    AddTripPlanDto dto = AddTripPlanDto.builder()
+        .title(request.getTitle())
+        .build();
+
+    Long savedTripPlanId = tripPlanService.registerTripPlan(loginId, dto);
+
+    return savedTripPlanId;
+  }
+
+}
