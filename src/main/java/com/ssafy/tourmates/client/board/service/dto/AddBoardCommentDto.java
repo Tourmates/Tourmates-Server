c84a6ae7f54a3debb4b46395805d3f094e09f3ab
정유빈 <youbeen2798@naver.com>
2023-05-14 21:18:04 +0900

    feat: AddBoardCommentDto 클래스 추가

---
+++ src/main/java/com/ssafy/tourmates/client/board/service/dto/AddBoardCommentDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.tourmates.client.board.service.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class AddBoardCommentDto {
+
+  private String content;
+
+  @Builder
+  public AddBoardCommentDto(String content) {
+    this.content = content;
+  }
+
+}
