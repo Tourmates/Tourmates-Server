415934b7674ee3a09b90b8ffd97cdc7ddb288249
youbeen2798 <youbeen2798@naver.com>
2023-05-15 14:26:58 +0900

    refactor: TripPlanService 수정 메소드 추가

--- src/main/java/com/ssafy/tourmates/client/tripPlan/service/TripPlanService.java
+++ src/main/java/com/ssafy/tourmates/client/tripPlan/service/TripPlanService.java
@@ -1,11 +1,13 @@
 package com.ssafy.tourmates.client.tripPlan.service;
 
 import com.ssafy.tourmates.client.tripPlan.service.dto.AddTripPlanDto;
+import com.ssafy.tourmates.client.tripPlan.service.dto.EditTripPlanDto;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public interface TripPlanService {
 
-  Long registerTripPlan(String loginId, AddTripPlanDto dto);
+    Long registerTripPlan(String loginId, AddTripPlanDto dto);
 
+    Long editTripPlan(Long tripPlanId, EditTripPlanDto dto);
 }
