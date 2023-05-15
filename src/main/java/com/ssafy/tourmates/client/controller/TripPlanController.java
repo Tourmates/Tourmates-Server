c96463022d5a5a5fa5865e23bf2a2d7652eade20
Chaos0103 <lyt1228@naver.com>
2023-05-15 11:23:57 +0900

    style: 불필요 import 제거

--- src/main/java/com/ssafy/tourmates/client/controller/TripPlanController.java
+++ src/main/java/com/ssafy/tourmates/client/controller/TripPlanController.java
@@ -9,7 +9,6 @@ import io.swagger.annotations.ApiOperation;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.graalvm.compiler.core.common.type.ArithmeticOpTable.BinaryOp.Add;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
