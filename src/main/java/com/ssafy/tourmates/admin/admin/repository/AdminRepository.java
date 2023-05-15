a7146ea2aa31d82719a0a6a6f1225201743761e3
chaos0103 <lyt1228@naver.com>
2023-05-15 23:05:14 +0900

    feat: 관리자 로그인용 쿼리 구현

--- src/main/java/com/ssafy/tourmates/admin/admin/repository/AdminRepository.java
+++ src/main/java/com/ssafy/tourmates/admin/admin/repository/AdminRepository.java
@@ -2,6 +2,11 @@ package com.ssafy.tourmates.admin.admin.repository;
 
 import com.ssafy.tourmates.admin.admin.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    Optional<Admin> findByLoginIdAndLoginPw(@Param("loginId") String loginId, @Param("loginPw") String loginPw);
 }
