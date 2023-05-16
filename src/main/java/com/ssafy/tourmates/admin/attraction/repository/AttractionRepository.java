34deea0fde54a197d4e0bb89eb9e3f9edadd52af
정유빈 <youbeen2798@naver.com>
2023-05-16 16:51:39 +0900

    feat: AttractionRepository 클래스 PK 재설정

--- src/main/java/com/ssafy/tourmates/admin/attraction/repository/AttractionRepository.java
+++ src/main/java/com/ssafy/tourmates/admin/attraction/repository/AttractionRepository.java
@@ -3,5 +3,5 @@ package com.ssafy.tourmates.admin.attraction.repository;
 import com.ssafy.tourmates.admin.attraction.AttractionInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AttractionRepository extends JpaRepository<AttractionInfo, Long>, AttractionRepositoryCustom {
+public interface AttractionRepository extends JpaRepository<AttractionInfo, Integer>, AttractionRepositoryCustom {
 }
