905c4280d382dbd8751950b899b0c698316de431
chaos0103 <lyt1228@naver.com>
2023-05-17 11:34:48 +0900

    feat: 질문 등록 컨트롤러 구현

---
+++ src/main/java/com/ssafy/tourmates/client/controller/QuestionController.java
@@ -0,0 +1,38 @@
+package com.ssafy.tourmates.client.controller;
+
+import com.ssafy.tourmates.client.controller.dto.question.request.AddQuestionRequest;
+import com.ssafy.tourmates.client.question.service.QuestionService;
+import com.ssafy.tourmates.client.question.service.dto.AddQuestionDto;
+import com.ssafy.tourmates.jwt.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/questions")
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @PostMapping("/register")
+    public Long registerQuestion(@Valid @RequestBody AddQuestionRequest request) {
+        String loginId = SecurityUtil.getCurrentLoginId();
+
+        AddQuestionDto dto = AddQuestionDto.builder()
+                .type(request.getType())
+                .title(request.getTitle())
+                .content(request.getContent())
+                .build();
+
+        Long questionId = questionService.registerQuestion(loginId, dto);
+        log.debug("questionId={}", questionId);
+        return questionId;
+    }
+}
