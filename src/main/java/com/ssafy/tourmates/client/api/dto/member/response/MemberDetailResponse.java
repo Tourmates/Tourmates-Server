bb9e6540f93c490500ecb755ae9f3aafc8d0b59c
정유빈 <youbeen2798@naver.com>
2023-05-20 21:08:05 +0900

    feat: MemberDetailResponse 클래스 구현

---
+++ src/main/java/com/ssafy/tourmates/client/api/dto/member/response/MemberDetailResponse.java
@@ -0,0 +1,27 @@
+package com.ssafy.tourmates.client.api.dto.member.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class MemberDetailResponse {
+
+    private String nickname;
+    private String birth;
+    private String emailId;
+    private String emailDomain;
+    private String startPhoneNumber;
+    private String middlePhoneNumber;
+    private String endPhoneNumber;
+
+    @Builder
+    public MemberDetailResponse(String nickname, String birth, String emailId, String emailDomain, String startPhoneNumber, String middlePhoneNumber, String endPhoneNumber) {
+        this.nickname = nickname;
+        this.birth = birth;
+        this.emailId = emailId;
+        this.emailDomain = emailDomain;
+        this.startPhoneNumber = startPhoneNumber;
+        this.middlePhoneNumber = middlePhoneNumber;
+        this.endPhoneNumber = endPhoneNumber;
+    }
+}
