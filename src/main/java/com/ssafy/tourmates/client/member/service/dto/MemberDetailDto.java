ffae7057b2e5fc7f5a0cb69e49f8e6c18f549883
정유빈 <youbeen2798@naver.com>
2023-05-20 21:07:40 +0900

    feat: MemberDetailDto 클래스 구현

---
+++ src/main/java/com/ssafy/tourmates/client/member/service/dto/MemberDetailDto.java
@@ -0,0 +1,27 @@
+package com.ssafy.tourmates.client.member.service.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class MemberDetailDto {
+
+    private String nickname;
+    private String birth;
+    private String emailId;
+    private String emailDomain;
+    private String startPhoneNumber;
+    private String middlePhoneNumber;
+    private String endPhoneNumber;
+
+    @Builder
+    public MemberDetailDto(String nickname, String birth, String emailId, String emailDomain, String startPhoneNumber, String middlePhoneNumber, String endPhoneNumber) {
+        this.nickname = nickname;
+        this.birth = birth;
+        this.emailId = emailId;
+        this.emailDomain = emailDomain;
+        this.startPhoneNumber = startPhoneNumber;
+        this.middlePhoneNumber = middlePhoneNumber;
+        this.endPhoneNumber = endPhoneNumber;
+    }
+}
