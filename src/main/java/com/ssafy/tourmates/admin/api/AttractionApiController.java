73453214eae203348e9800eaca4aa6117421b65a
chaos0103 <lyt1228@naver.com>
2023-05-22 07:52:28 +0900

    refactor: 관광지 정보 조회 컨트롤러 구현

--- src/main/java/com/ssafy/tourmates/admin/api/AttractionApiController.java
+++ src/main/java/com/ssafy/tourmates/admin/api/AttractionApiController.java
@@ -1,17 +1,20 @@
 package com.ssafy.tourmates.admin.api;
 
-import com.ssafy.tourmates.admin.api.dto.attraction.response.AttractionSearchResponse;
-import com.ssafy.tourmates.admin.attraction.service.AttractionQueryService;
 import com.ssafy.tourmates.admin.api.dto.attraction.response.AttractionResponse;
+import com.ssafy.tourmates.admin.api.dto.attraction.response.AttractionSearchResponse;
+import com.ssafy.tourmates.admin.api.dto.attraction.response.GugunResponse;
+import com.ssafy.tourmates.admin.api.dto.attraction.response.SidoResponse;
+import com.ssafy.tourmates.admin.attraction.repository.dto.AttractionSearchCondition;
+import com.ssafy.tourmates.admin.attraction.service.AttractionQueryService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -21,6 +24,38 @@ import java.util.List;
 public class AttractionApiController {
 
     private final AttractionQueryService attractionQueryService;
+
+    @GetMapping
+    public Result<List<AttractionResponse>> searchByCondition(
+            @RequestParam String keyword,
+            @RequestParam Integer sidoCode,
+            @RequestParam Integer gugunCode,
+            @RequestParam(defaultValue = "0") Integer contentTypeId
+    ) {
+        AttractionSearchCondition condition = AttractionSearchCondition.builder()
+                .keyword(keyword)
+                .sidoCode(sidoCode)
+                .gugunCode(gugunCode)
+                .contentTypeId(contentTypeId)
+                .build();
+        List<AttractionResponse> responses = attractionQueryService.searchByCondition(condition);
+        log.debug("responses size={}", responses.size());
+        return new Result<>(responses);
+    }
+
+    @GetMapping("/sido")
+    public Result<?> getSido() {
+        List<SidoResponse> responses = attractionQueryService.searchSido();
+        log.debug("responses size={}", responses.size());
+        return new Result<>(responses);
+    }
+
+    @GetMapping("/gugun")
+    public Result<?> getGugun(@RequestParam Integer sidoCode) {
+        List<GugunResponse> responses = attractionQueryService.searchGugun(sidoCode);
+        log.debug("responses size={}", responses.size());
+        return new Result<>(responses);
+    }
 
     @GetMapping("/search")
     public Result<List<AttractionSearchResponse>> searchAttraction() {
