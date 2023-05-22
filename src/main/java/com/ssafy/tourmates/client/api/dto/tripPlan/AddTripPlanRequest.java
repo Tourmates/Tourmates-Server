f0879740cb75926a9aa55d55b1099b533431f67f
chaos0103 <lyt1228@naver.com>
2023-05-23 01:57:51 +0900

    refactor: 컨트롤러 관광지 데이터 추가

--- src/main/java/com/ssafy/tourmates/client/api/dto/tripPlan/AddTripPlanRequest.java
+++ src/main/java/com/ssafy/tourmates/client/api/dto/tripPlan/AddTripPlanRequest.java
@@ -4,10 +4,13 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class AddTripPlanRequest {
 
     @NotBlank
     @Size(max = 50)
     private String title;
+    private List<Integer> contentIds;
 }
