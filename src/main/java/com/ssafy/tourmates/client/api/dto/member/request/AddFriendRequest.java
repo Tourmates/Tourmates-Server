5f683b36ec74cb4c8cbe72b927be10fd915c09a5
youbeen2798 <youbeen2798@naver.com>
2023-05-23 16:22:13 +0900

    feat: AddFriendRequest 구현 완료

---
+++ src/main/java/com/ssafy/tourmates/client/api/dto/member/request/AddFriendRequest.java
@@ -0,0 +1,12 @@
+package com.ssafy.tourmates.client.api.dto.member.request;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class AddFriendRequest {
+
+    @NotBlank
+    private Long targetId;
+}
