bf7c0931bf0baba2e872b47c931353cf9d6add25
youbeen2798 <youbeen2798@naver.com>
2023-05-23 17:47:25 +0900

    fix: Friend entity 수정 구현 완료

--- src/main/java/com/ssafy/tourmates/client/friend/Friend.java
+++ src/main/java/com/ssafy/tourmates/client/friend/Friend.java
@@ -1,15 +1,14 @@
 package com.ssafy.tourmates.client.friend;
 
+import com.ssafy.tourmates.client.member.Member;
 import com.ssafy.tourmates.common.domain.TimeBaseEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+import static javax.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PROTECTED;
 
 @Entity
@@ -21,15 +20,19 @@ public class Friend extends TimeBaseEntity {
     @GeneratedValue
     @Column(name = "friend_id")
     private Long id;
-    @Column(nullable = false, name = "member_id")
-    private Long memberId;
-    @Column(nullable = false, name = "target_id")
-    private Long targetId;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(referencedColumnName = "member_id", name = "member_id")
+    private Member loginMember;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(referencedColumnName = "member_id", name = "target_id")
+    private Member targetMember;
 
     @Builder
-    public Friend(Long id, Long memberId, Long targetId){
+    public Friend(Long id, Member loginMember, Member targetMember){
         this.id = id;
-        this.memberId = memberId;
-        this.targetId = targetId;
+        this.loginMember = loginMember;
+        this.targetMember = targetMember;
     }
 }
