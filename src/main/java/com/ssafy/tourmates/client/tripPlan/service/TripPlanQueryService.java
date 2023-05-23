d992153e49a6f0cd98a0baf204aeeb9e236209a1
Chaos0103 <lyt1228@naver.com>
2023-05-23 10:22:02 +0900

    feat: 여행계획 목록 조회 서비스 구현

---
+++ src/main/java/com/ssafy/tourmates/client/tripPlan/service/TripPlanQueryService.java
@@ -0,0 +1,16 @@
+package com.ssafy.tourmates.client.tripPlan.service;
+
+import com.ssafy.tourmates.client.api.dto.tripplan.response.PlanResponse;
+import com.ssafy.tourmates.client.tripPlan.repository.dto.PlanSearchCondition;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+public interface TripPlanQueryService {
+
+    List<PlanResponse> searchByCondition(PlanSearchCondition condition, Pageable pageable);
+
+    Long getTotalCount();
+}
