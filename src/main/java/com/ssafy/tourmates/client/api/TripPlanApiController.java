94312359dc5eac2701bb03fe315525123e7c04d3
Chaos0103 <lyt1228@naver.com>
2023-05-23 10:22:06 +0900

    feat: 여행계획 목록 조회 컨트롤러 구현

--- src/main/java/com/ssafy/tourmates/client/api/TripPlanApiController.java
+++ src/main/java/com/ssafy/tourmates/client/api/TripPlanApiController.java
@@ -3,18 +3,26 @@ package com.ssafy.tourmates.client.api;
 import com.ssafy.tourmates.client.api.dto.tripplan.request.AddDetailPlanRequest;
 import com.ssafy.tourmates.client.api.dto.tripplan.request.AddTripPlanRequest;
 import com.ssafy.tourmates.client.api.dto.tripplan.request.EditTripPlanRequest;
+import com.ssafy.tourmates.client.api.dto.tripplan.response.PlanResponse;
+import com.ssafy.tourmates.client.tripPlan.repository.dto.PlanSearchCondition;
 import com.ssafy.tourmates.client.tripPlan.service.DetailTripPlanService;
+import com.ssafy.tourmates.client.tripPlan.service.TripPlanQueryService;
 import com.ssafy.tourmates.client.tripPlan.service.TripPlanService;
 import com.ssafy.tourmates.client.tripPlan.service.dto.*;
 import com.ssafy.tourmates.jwt.SecurityUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+
+import static com.ssafy.tourmates.client.tripPlan.repository.dto.PlanSearchCondition.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,12 +32,41 @@ import java.util.List;
 public class TripPlanApiController {
 
     private final TripPlanService tripPlanService;
+    private final TripPlanQueryService tripPlanQueryService;
     private final DetailTripPlanService detailTripPlanService;
 
     @ApiOperation(value = "여행계획 조회")
     @GetMapping
-    public void searchTripPlans() {
+    public Result<?> searchTripPlans(
+        @RequestParam(defaultValue = "") String keyword,
+        @RequestParam(defaultValue = "0") Integer type,
+        @RequestParam(defaultValue = "1") Integer pageNumber
+    ) {
+        //0: 타이틀, 1: 닉네임, 2: 관광지이름
+        PlanSearchConditionBuilder builder = builder();
+        switch (type) {
+            case 1:
+                builder.nickname(keyword);
+                break;
+            case 2:
+                builder.attractionTitle(keyword);
+                break;
+            default:
+                builder.title(keyword);
+                break;
+        }
+        PlanSearchCondition condition = builder.build();
+        PageRequest pageRequest = PageRequest.of(pageNumber / 10, 10);
 
+        List<PlanResponse> responses = tripPlanQueryService.searchByCondition(condition, pageRequest);
+        log.debug("size={}", responses.size());
+        return new Result<>(responses);
+    }
+
+    @ApiOperation(value = "여행계획 총 갯수 조회")
+    @GetMapping("/totalCount")
+    public Long totalCount() {
+        return tripPlanQueryService.getTotalCount();
     }
 
     @ApiOperation(value = "여행계획 등록")
@@ -81,5 +118,11 @@ public class TripPlanApiController {
     public Integer removeDetailTripPlan(@PathVariable Long tripPlanId, @PathVariable Long detailTripPlanId) {
         detailTripPlanService.removeDetailTripPlan(detailTripPlanId);
         return 1;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
     }
 }
