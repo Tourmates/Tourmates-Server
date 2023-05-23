bec2b663b61680b052c178b7f98b5409d594c0d4
Chaos0103 <lyt1228@naver.com>
2023-05-23 16:24:48 +0900

    feat: 트랜드 도메일 설계

---
+++ src/main/java/com/ssafy/tourmates/admin/trend/Trend.java
@@ -0,0 +1,42 @@
+package com.ssafy.tourmates.admin.trend;
+
+import com.ssafy.tourmates.admin.attraction.AttractionInfo;
+import com.ssafy.tourmates.common.domain.TimeBaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Trend extends TimeBaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "trend_id")
+    private Long id;
+    private int teenage;
+    private int twenty;
+    private int thirty;
+    private int male;
+    private int female;
+
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "content_id")
+    private AttractionInfo attractionInfo;
+
+    @Builder
+    public Trend(Long id, int teenage, int twenty, int thirty, int male, int female, AttractionInfo attractionInfo) {
+        this.id = id;
+        this.teenage = teenage;
+        this.twenty = twenty;
+        this.thirty = thirty;
+        this.male = male;
+        this.female = female;
+        this.attractionInfo = attractionInfo;
+    }
+}
