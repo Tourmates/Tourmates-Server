5974dc5f6063623b24af5e8c6bfe2c69d344ad1d
youbeen2798 <youbeen2798@naver.com>
2023-05-24 15:39:31 +0900

    feat: HotPlaceCommentRepository 여행 댓글 등록 기능 구현

--- src/main/java/com/ssafy/tourmates/client/hotplace/repository/HotPlaceCommentRepository.java
+++ src/main/java/com/ssafy/tourmates/client/hotplace/repository/HotPlaceCommentRepository.java
@@ -2,6 +2,10 @@ package com.ssafy.tourmates.client.hotplace.repository;
 
 import com.ssafy.tourmates.client.hotplace.HotPlaceComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface HotPlaceCommentRepository extends JpaRepository<HotPlaceComment, Long> {
+    List<HotPlaceComment> findAllByHotPlaceId(@Param("hotPlaceId") Long hotPlaceId);
 }
