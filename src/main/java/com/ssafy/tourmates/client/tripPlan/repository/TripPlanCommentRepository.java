268a125ccf23a75fec8f07b61a9d7d0782caf161
youbeen2798 <youbeen2798@naver.com>
2023-05-24 14:44:49 +0900

    feat: TripPlanCommentRepository 여행 계획 댓글 기능 구현 완료

--- src/main/java/com/ssafy/tourmates/client/tripPlan/repository/TripPlanCommentRepository.java
+++ src/main/java/com/ssafy/tourmates/client/tripPlan/repository/TripPlanCommentRepository.java
@@ -2,6 +2,11 @@ package com.ssafy.tourmates.client.tripPlan.repository;
 
 import com.ssafy.tourmates.client.tripPlan.TripPlanComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TripPlanCommentRepository extends JpaRepository<TripPlanComment, Long> {
+
+    List<TripPlanComment> findAllByTripPlanId(@Param("tripPlanId") Long tripPlanId);
 }
