54fd65ca92725a5e174d6705a9356d1ac6f29e1d
youbeen2798 <youbeen2798@naver.com>
2023-05-25 17:57:21 +0900

    feat: EditHotPlaceResponse 해시태그 추가

--- src/main/java/com/ssafy/tourmates/client/api/dto/hotplace/response/EditHotPlaceResponse.java
+++ src/main/java/com/ssafy/tourmates/client/api/dto/hotplace/response/EditHotPlaceResponse.java
@@ -3,29 +3,32 @@ package com.ssafy.tourmates.client.api.dto.hotplace.response;
 import com.ssafy.tourmates.common.domain.ContentType;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class EditHotPlaceResponse {
 
     private Long hotPlaceId;
-    private ContentType tag;
     private String title;
     private String visitedDate;
     private String content;
-
     private String attractionTitle;
     private Double latitude;
     private Double longitude;
+    private List<String> hashtags;
 
     @Builder
-    public EditHotPlaceResponse(Long hotPlaceId, ContentType tag, String title, String visitedDate, String content, String attractionTitle, Double latitude, Double longitude) {
+    public EditHotPlaceResponse(Long hotPlaceId, String title, String visitedDate, String content, String attractionTitle, Double latitude, Double longitude, List<String> hashtags) {
         this.hotPlaceId = hotPlaceId;
-        this.tag = tag;
         this.title = title;
         this.visitedDate = visitedDate;
         this.content = content;
         this.attractionTitle = attractionTitle;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.hashtags = hashtags;
     }
 }
